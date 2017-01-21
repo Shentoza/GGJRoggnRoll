2bd1ff95e02be630b0672dbb5399994a3e88ba7f
Sebastian Pauli <sebastian.pauli@outlook.com>
2017-01-21 11:25:06 +0100

    Basic Initial Stuff (Sound Source, Bread Control, etc.)

---
+++ Assets/Scripts/Bread/BreadControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreadControl : MonoBehaviour
+{
+	public SoundSource sound;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void OnCollisionEnter( Collision collision )
+	{
+		Debug.Log( "Collision: " + collision );
+
+		sound.Emit( transform.position );
+	}
+
+	public void Launch( Vector3 position, Vector3 direction )
+	{
+		transform.position = position;
+		transform.LookAt( position + direction );
+		transform.Rotate( Vector3.up, 90 );
+
+		GetComponent<Rigidbody>().AddForce( direction * 1000 );
+	}
+}
